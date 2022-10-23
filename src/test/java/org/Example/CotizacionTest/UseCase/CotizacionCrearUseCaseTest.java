7fe07ef22e08e8fed1b8d27a421492e1390d0702
Hugo Fajardo <hugofajardo1@gmail.com>
2022-10-23 09:00:37 -0300

    CotizacionCrearUseCaseTest

---
+++ src/test/java/org/Example/CotizacionTest/UseCase/CotizacionCrearUseCaseTest.java
@@ -0,0 +1,8 @@
+package org.Example.CotizacionTest.UseCase;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+
+@ExtendWith(mockito.MockitoExtension.class)
+public class CotizacionCrearUseCaseTest {
+
+}
